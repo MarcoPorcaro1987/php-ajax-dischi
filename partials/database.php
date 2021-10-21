9e407cbaaf97d3bed36c6e3ec0cb3f77fadb587c
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-10-21 15:51:24 +0100

    fix link cover album iron maden

--- partials/database.php
+++ partials/database.php
@@ -28,7 +28,7 @@ $database = [
 		'title' => 'Brave new World',
 		'author' => 'Iron Maiden',
 		'year' => 2000,
-		'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
+		'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg'
 	],
 	[
 		'title' => 'One more car, one more rider',
